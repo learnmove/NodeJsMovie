bf6b097beeb4658509233d6b79f86fa3272174d4
e <f>
2017-04-13 14:49:38 +0800

    設定好讀取bower裡的css js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express=require('express');
 var port =process.env.PORT||3000;
 var app = express();
-app.set('views','./imoc/views');
+app.set('views','./views');
 app.set('view engine','jade');
 app.use(express.static(__dirname))
+app.use('/bower_components',express.static(__dirname+'/bower_components'));
+
 app.listen(port);
 console.log('server start ',port);
 // index
